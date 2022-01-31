28b9a6a85630a6d854d782cbc4be3b28dbd03015
Ka Hou, CHOI <kz1412@link.cuhk.edu.hk>
2022-01-31 19:46:34 +0800

    fix: add check if master file exists

--- touch_atm_project/atms.cob
+++ touch_atm_project/atms.cob
@@ -52,8 +52,9 @@
 
        WORKING-STORAGE SECTION. 
        01  MASTER-FILE-STATUS               PIC 99.
-           88 MASTER-FILE-ALREADY-OPEN      VALUE 41.
            88 MASTER-FILE-EOF-REACHED       VALUE 10.
+           88 MASTER-FILE-NOT-FOUND         VALUE 35.
+           88 MASTER-FILE-ALREADY-OPEN      VALUE 41.
        01  T71-ONE-FILE-STATUS              PIC 99.
            88 T71-ONE-FILE-ALREADY-OPEN     VALUE 41.
        01  T71-THREE-FILE-STATUS            PIC 99.
@@ -64,8 +65,8 @@
            88 ATM-71-THREE                  VALUE 2.
 
        01  VALIDATE-ACCT-INFO-FOR           PIC 9.
-           88 VALIDATING-USER-INFO          VALUE 1.
-           88 VALIDATING-TARGET-INFO        VALUE 2.
+           88 VALIDATING-USER               VALUE 1.
+           88 VALIDATING-TARGET             VALUE 2.
 
        01  USER-ACCT-RECORD.
            02 USER-ACCT-INFO.
@@ -80,15 +81,16 @@
 
        01  TARGET-ACCT-NUNBER               PIC 9(16).
 
-       01  TRANSACTION-AMOUNT               PIC 9(5)V9(2).
+       01  TRANSAC-AMOUNT                   PIC 9(5)V9(2).
 
-       01  TRANSACTION-RECORD.
-           02 T-REC-ACCT-NUMBER             PIC 9(16).
-           02 T-REC-OPERATION               PIC A.
-           02 T-REC-AMOUNT                  PIC 9(5)V9(2).
-           02 T-REC-TIMESTAMP               PIC 9(5) VALUE 0.
+       01  TRANSAC-BUFFER.
+           02 TRANSAC-BUF-ACCT-NUMBER       PIC 9(16).
+           02 TRANSAC-BUF-OPERATION         PIC A.
+           02 TRANSAC-BUF-AMOUNT            PIC 9(5)V9(2).
+           02 TRANSAC-BUF-TIMESTAMP         PIC 9(5) VALUE 0.
 
-       01  TRANSFER-RECORD-STATUS           PIC 9.
+       01  TRANSFER-STATUS                  PIC 9 VALUE 0.
+           88 INITIAL-TRANSFER-STATUS       VALUE 0.
            88 WITHDRAWED                    VALUE 1.
            88 DEPOSITED                     VALUE 2.
 
@@ -97,22 +99,19 @@
            88 CONTINUE-NO                   VALUE "N".
 
        PROCEDURE DIVISION.
-       MAIN.
-           IF NOT T71-ONE-FILE-ALREADY-OPEN 
+       CHECK-MASTER-FILE-EXISTS.
+           OPEN INPUT MASTER-FILE.
+
+           IF MASTER-FILE-NOT-FOUND
            THEN 
-              OPEN OUTPUT T71-ONE-FILE 
-           END-IF.
-           
-           IF NOT T71-THREE-FILE-ALREADY-OPEN 
-           THEN 
-              OPEN OUTPUT T71-THREE-FILE 
+              DISPLAY "[ABORT]: MASTER FILE NOT FOUND"
+              STOP RUN
            END-IF.
 
            GO TO ATM-INITIALIZE.
 
-           STOP RUN.
-
        ATM-INITIALIZE.
+           DISPLAY SPACES 
            DISPLAY "##############################################"
            DISPLAY "##         Gringotts Wizarding Bank         ##"
            DISPLAY "##                 Welcome                  ##"
@@ -125,12 +124,7 @@
            DISPLAY "=> PRESS 2 FOR ATM 713"
            ACCEPT USER-ATM-CHOICE.
 
-           IF ATM-71-ONE 
-           THEN 
-              GO TO INPUT-ACCT-INFO 
-           END-IF.
-
-           IF ATM-71-THREE 
+           IF ATM-71-ONE OR ATM-71-THREE
            THEN 
               GO TO INPUT-ACCT-INFO 
            END-IF.
@@ -145,14 +139,14 @@
            DISPLAY "=> PASSWORD"
            ACCEPT USER-ACCT-PASSWORD.
            
-           MOVE 1 TO VALIDATE-ACCT-INFO-FOR.
+           SET VALIDATING-USER TO TRUE.
            GO TO VALIDATE-ACCT-INFO.
 
        CHOOSE-SERVICE.
-           DISPLAY "PLEASE CHOOSE YOUR SERVICE"
-           DISPLAY "PRESS D FOR DEPOSIT"
-           DISPLAY "PRESS W FOR WITHDRAWAL"
-           DISPLAY "PRESS T FOR TRANSFER"
+           DISPLAY "=> PLEASE CHOOSE YOUR SERVICE"
+           DISPLAY "=> PRESS D FOR DEPOSIT"
+           DISPLAY "=> PRESS W FOR WITHDRAWAL"
+           DISPLAY "=> PRESS T FOR TRANSFER"
            ACCEPT USER-SERVICE-CHOICE.
 
            IF DEPOSIT OR WITHDRAWAL 
@@ -170,30 +164,23 @@
 
        INPUT-AMOUNT.
            DISPLAY "=> AMOUNT"
-           ACCEPT TRANSACTION-AMOUNT.
+           ACCEPT TRANSAC-AMOUNT.
 
-           DISPLAY USER-ACCT-BALANCE SPACE TRANSACTION-AMOUNT
+           DISPLAY USER-ACCT-BALANCE SPACE TRANSAC-AMOUNT.
 
-           IF TRANSACTION-AMOUNT < 0
+           IF TRANSAC-AMOUNT < 0
            THEN 
               DISPLAY "=> INVALID INPUT"
               GO TO INPUT-AMOUNT
-           END-IF
-
-           IF DEPOSIT 
-           THEN 
-              GO TO GENERATE-TRANSACTION-RECORD
            END-IF.
 
-           IF WITHDRAWAL OR TRANSFER 
+           IF NOT DEPOSIT AND TRANSAC-AMOUNT > USER-ACCT-BALANCE 
            THEN 
-              IF TRANSACTION-AMOUNT > USER-ACCT-BALANCE 
-              THEN 
-                 DISPLAY "INSUFFICIENT BALANCE"
-                 GO TO INPUT-AMOUNT 
-              END-IF 
-              GO TO GENERATE-TRANSACTION-RECORD
+              DISPLAY "=> INSUFFICIENT BALANCE"
+              GO TO INPUT-AMOUNT 
            END-IF.
+
+           GO TO GENERATE-TRANSAC-RECORD.
 
        INPUT-TARGET-INFO.
            DISPLAY "=> TARGET ACCOUT"
@@ -201,73 +188,82 @@
 
            IF TARGET-ACCT-NUNBER = USER-ACCT-NUMBER 
            THEN 
-              DISPLAY "YOU CANNOT TRANSFER TO YOURSELF"
+              DISPLAY "=> YOU CANNOT TRANSFER TO YOURSELF"
               GO TO INPUT-TARGET-INFO 
-           END-IF .
+           END-IF.
 
-           MOVE 2 TO VALIDATE-ACCT-INFO-FOR.
+           SET VALIDATING-TARGET TO TRUE.
            GO TO VALIDATE-ACCT-INFO.
 
-       GENERATE-TRANSACTION-RECORD.
+       GENERATE-TRANSAC-RECORD.
            IF DEPOSIT OR WITHDRAWAL 
            THEN 
-              MOVE USER-ACCT-NUMBER TO T-REC-ACCT-NUMBER
-              MOVE USER-SERVICE-CHOICE TO T-REC-OPERATION
-              MOVE TRANSACTION-AMOUNT TO T-REC-AMOUNT
-              DISPLAY USER-ATM-CHOICE ": " TRANSACTION-RECORD
+              MOVE USER-ACCT-NUMBER      TO TRANSAC-BUF-ACCT-NUMBER
+              MOVE USER-SERVICE-CHOICE   TO TRANSAC-BUF-OPERATION
+              MOVE TRANSAC-AMOUNT        TO TRANSAC-BUF-AMOUNT
+              DISPLAY USER-ATM-CHOICE ": " TRANSAC-BUFFER
               GO TO WRITE-ATM-RECORD
            END-IF.
            
-           IF TRANSFER
+           IF TRANSFER 
            THEN 
               IF NOT WITHDRAWED 
               THEN 
-                 MOVE USER-ACCT-NUMBER TO T-REC-ACCT-NUMBER
-                 MOVE "W" TO T-REC-OPERATION
-                 MOVE TRANSACTION-AMOUNT TO T-REC-AMOUNT
-                 DISPLAY USER-ATM-CHOICE ": " TRANSACTION-RECORD 
-                 MOVE 1 TO TRANSFER-RECORD-STATUS
+                 MOVE USER-ACCT-NUMBER   TO TRANSAC-BUF-ACCT-NUMBER
+                 MOVE "W"                TO TRANSAC-BUF-OPERATION
+                 MOVE TRANSAC-AMOUNT     TO TRANSAC-BUF-AMOUNT
+                 DISPLAY USER-ATM-CHOICE ": " TRANSAC-BUFFER 
+                 SET WITHDRAWED TO TRUE 
                  GO TO WRITE-ATM-RECORD
               END-IF
-
-              MOVE TARGET-ACCT-NUNBER TO T-REC-ACCT-NUMBER
-              MOVE "D" TO T-REC-OPERATION
-              MOVE TRANSACTION-AMOUNT TO T-REC-AMOUNT
-              DISPLAY USER-ATM-CHOICE ": " TRANSACTION-RECORD 
-              MOVE 2 TO TRANSFER-RECORD-STATUS
-              GO TO WRITE-ATM-RECORD
+              IF WITHDRAWED 
+              THEN 
+                 MOVE TARGET-ACCT-NUNBER TO TRANSAC-BUF-ACCT-NUMBER
+                 MOVE "D"                TO TRANSAC-BUF-OPERATION
+                 MOVE TRANSAC-AMOUNT     TO TRANSAC-BUF-AMOUNT
+                 DISPLAY USER-ATM-CHOICE ": " TRANSAC-BUFFER 
+                 SET DEPOSITED TO TRUE
+                 GO TO WRITE-ATM-RECORD
+              END-IF
            END-IF.
 
        WRITE-ATM-RECORD.
-           IF ATM-71-ONE
+           IF 
+              NOT T71-ONE-FILE-ALREADY-OPEN OR 
+              NOT T71-THREE-FILE-ALREADY-OPEN 
            THEN 
-              WRITE T71-ONE-RECORD FROM TRANSACTION-RECORD
-              ADD 1 TO T-REC-TIMESTAMP
+              OPEN OUTPUT T71-ONE-FILE, T71-THREE-FILE 
            END-IF.
 
-           IF ATM-71-THREE
+           IF ATM-71-ONE 
+           THEN 
+              WRITE T71-ONE-RECORD FROM TRANSAC-BUFFER
+              ADD 1 TO TRANSAC-BUF-TIMESTAMP 
+           END-IF.
+
+           IF ATM-71-THREE 
            THEN
-              WRITE T71-THREE-RECORD FROM TRANSACTION-RECORD
-              ADD 1 TO T-REC-TIMESTAMP
+              WRITE T71-THREE-RECORD FROM TRANSAC-BUFFER
+              ADD 1 TO TRANSAC-BUF-TIMESTAMP 
            END-IF.
 
-           IF TRANSFER AND NOT DEPOSITED
+           IF TRANSFER AND NOT DEPOSITED 
            THEN 
-              GO TO GENERATE-TRANSACTION-RECORD
+              GO TO GENERATE-TRANSAC-RECORD
            END-IF.
            
-           MOVE 0 TO TRANSFER-RECORD-STATUS
+           SET INITIAL-TRANSFER-STATUS TO TRUE.
            GO TO CHOOSE-IF-CONTINUE.           
 
        CHOOSE-IF-CONTINUE.
            DISPLAY "=> CONTINUE?"
-           DISPLAY "=>  N FOR NO"
-           DISPLAY "=>  Y FOR YES"
+           DISPLAY "=> N FOR NO"
+           DISPLAY "=> Y FOR YES"
            ACCEPT IF-CONTINUE-CHOICE.
 
            IF CONTINUE-YES
            THEN 
-              GO TO MAIN
+              GO TO ATM-INITIALIZE 
            END-IF.
 
            IF NOT CONTINUE-NO 
@@ -280,7 +276,8 @@
            STOP RUN.
 
        VALIDATE-ACCT-INFO.
-           IF NOT MASTER-FILE-ALREADY-OPEN THEN 
+           IF NOT MASTER-FILE-ALREADY-OPEN 
+           THEN 
               OPEN INPUT MASTER-FILE
            END-IF.
 
@@ -291,27 +288,27 @@
            IF MASTER-FILE-EOF-REACHED 
            THEN 
               CLOSE MASTER-FILE 
-              IF VALIDATING-USER-INFO 
+              IF VALIDATING-USER 
               THEN
-                 DISPLAY "INCORRECT ACCOUNT/PASSWORD"
+                 DISPLAY "=> INCORRECT ACCOUNT/PASSWORD"
                  GO TO INPUT-ACCT-INFO
               END-IF
-              IF VALIDATING-TARGET-INFO 
+              IF VALIDATING-TARGET 
               THEN
-                 DISPLAY "TARGET ACCOUNT DOES NOT EXIST"
+                 DISPLAY "=> TARGET ACCOUNT DOES NOT EXIST"
                  GO TO INPUT-TARGET-INFO
               END-IF
            END-IF.
            
-           IF VALIDATING-USER-INFO AND 
+           IF VALIDATING-USER AND 
               MSTR-ACCT-INFO = USER-ACCT-INFO
            THEN 
               DISPLAY MASTER-RECORD 
               IF ACCT-NEGATIVE 
               THEN 
-                 DISPLAY "NEGATIVE REMAINS TRANSACTION ABORT"
+                 DISPLAY "=> NEGATIVE REMAINS TRANSACTION ABORT"
                  CLOSE MASTER-FILE 
-                 GO TO MAIN
+                 GO TO ATM-INITIALIZE 
               END-IF 
               IF ACCT-POSITIVE
               THEN
@@ -320,7 +317,7 @@
               GO TO CHOOSE-SERVICE
            END-IF.
 
-           IF VALIDATING-TARGET-INFO AND 
+           IF VALIDATING-TARGET AND 
               MSTR-ACCT-NUMBER = TARGET-ACCT-NUNBER 
            THEN 
               CLOSE MASTER-FILE
